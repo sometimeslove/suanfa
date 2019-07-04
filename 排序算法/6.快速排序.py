e16190c099cb9604a61efa56e1245e3cdb0ccb74
sometimeslove <857508399@qq.com>
2019-07-04 22:17:41 +0800

    归并排序

--- 排序算法/6.快速排序.py
+++ 排序算法/6.快速排序.py
@@ -1,11 +1,32 @@
-import  random
+import random
 import numpy as np
 
-def xuanzhe(arr):
-	if len(arr)<2:
-		return
-	for i in range(0,len(arr)):
-		for j in range(i,len(arr)):
-			if arr[j]<arr[i]:
-				arr[i], arr[j] = arr[j], arr[i]
 
+def kuaisu(arr, left, right):
+    if left >= right:
+        return
+    middle = patition(arr, left, right)
+    kuaisu(arr, left, middle - 1)
+    kuaisu(arr, middle + 1, right)
+
+
+def patition(arr, left, right):
+    flag = left
+    for i in range(left + 1, right + 1):
+        if arr[i] < arr[left]:
+            arr[i], arr[flag] = arr[flag], arr[i]
+            ++flag
+    arr[left], arr[flag] = arr[flag], arr[left]
+    return flag
+
+
+if __name__ == '__main__':
+    arr = np.random.randint(1, 100, 20)
+    print(arr)
+    l = len(arr) - 1
+    kuaisu(arr, 0, l)
+    print(arr)
+    np.random.shuffle(arr)
+    print(arr)
+    kuaisu(arr, 0, l)
+    print(arr)
