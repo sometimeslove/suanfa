33f1c4961f052ac59b0af75576f43fe152f8603d
sometimeslove <857508399@qq.com>
2019-07-04 09:14:17 +0800

    提交快速排序测试文件

---
+++ 排序算法/6.快速排序.py
@@ -0,0 +1,20 @@
+import  random
+import numpy as np
+
+def xuanzhe(arr):
+	if len(arr)<2:
+		return
+	for i in range(0,len(arr)):
+		for j in range(i,len(arr)):
+			if arr[j]<arr[i]:
+				arr[i], arr[j] = arr[j], arr[i]
+
+if __name__ == '__main__':
+	arr = np.random.randint(1,100,20)
+	print(arr)
+	xuanzhe(arr)
+	print(arr)
+	np.random.shuffle(arr)
+	print(arr)
+	xuanzhe(arr)
+	print(arr)
